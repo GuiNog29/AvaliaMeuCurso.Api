11208b62ed01f80ff10e9d37ca1e27affdc37859
Guilherme Almeida <guimathiasgui@gmail.com>
2024-08-15 13:04:20 -0300

    Criando entidade e model

---
+++ AvaliaMeuCurso.Entities/Curso.cs
@@ -0,0 +1,10 @@
+﻿namespace AvaliaMeuCurso.Entities
+{
+    public class Curso
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public ICollection<Avaliacao> Avaliacoes { get; set; }
+    }
+}
