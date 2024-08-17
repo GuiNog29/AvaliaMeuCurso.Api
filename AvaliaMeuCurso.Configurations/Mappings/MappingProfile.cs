e78f4995d5fa7f46bc8c3b4f48f09c55febeee4a
Guilherme Almeida <guimathiasgui@gmail.com>
2024-08-17 13:42:12 -0300

    Criando mapeamento

---
+++ AvaliaMeuCurso.Configurations/Mappings/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using AvaliaMeuCurso.Models;
+
+namespace AvaliaMeuCurso.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Curso, CursoModel>().ReverseMap();
+            CreateMap<Curso, CursoAtualizacaoModel>().ReverseMap();
+
+            CreateMap<Estudante, EstudanteModel>().ReverseMap();
+            CreateMap<Estudante, EstudanteAtualizacaoModel>().ReverseMap();
+
+            CreateMap<Avaliacao, AvaliacaoModel>().ReverseMap();
+            CreateMap<Avaliacao, AvaliacaoAtualizacaoModel>().ReverseMap();
+        }
+    }
+}
